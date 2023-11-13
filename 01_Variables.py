aad77c534694a30bcce22806d2b93f980a3e96d4
Akame <suefuxbbdudjdurbfud@gmail.com>
2023-11-13 01:50:11 -0500

    12/11/23

--- 01_Variables.py
+++ 01_Variables.py
@@ -1,7 +1,6 @@
-# Variables
-
 # Buenas practicas de variables : first_name
 
+# Variables
 my_string = 'My String variable'
 print(my_string)
 
@@ -14,5 +13,39 @@ print(my_float)
 my_bool = True
 print(my_bool)
 
-print(my_int, my_string)
 
+# Concatenacion de variables en un print
+print(my_int, my_string)
+print('Este es el valor de: ', my_bool)
+
+# Algunas funciones del sistema
+print(len(my_string))
+
+
+# Variables en una sola linea. No abusar de esto!
+
+name, surname, alias, age = 'Esdeath', 'San', 'Esdeath-Dev', 22
+
+print('My name is:',name, surname +'. My age is: ',age, 'And my alias is: ',alias)
+
+
+# Input
+name = input('What is your nam: ')
+age = input('How old are u: ')
+print(name)
+print(age)
+
+# Cambiamos su tipo
+name = 32
+age = 'Esdeath'
+print(name)
+print(age)
+
+
+# Forzamos el tipo?
+address: str = "Mi direccion"
+address = 32
+address = True
+address = 1.2
+
+print(address)
---
+++ 02_operators.py
@@ -0,0 +1,43 @@
+### Operadores ###
+
+print (3 + 4)
+print (3 - 4)
+print (3 * 4)
+print (3 / 4)
+print (10 % 3)
+print (3 ^ 4)
+print (10 // 3)
+print(2 ** 3)
+
+print ('Hola ' + 'Python ' + 'Que tal? ')
+
+print ('Hola ' + str(5))
+
+
+### Operadores comparativos ###
+
+print (3 > 4)
+print (3  < 4)
+print (3 >= 4)
+print (3 <= 4)
+print (3 == 4)
+print (3 != 4)
+
+print('Hola' > 'HolaA')
+print('Hola' < 'HolaA')
+print('Hola' >= 'HolaA') # Ordenacion alfabeetica por ASCII
+print(len('Hola') >= len('HolaA')) # Cuenta caracteres
+print('Hola' <= 'HolaA')
+print('Hola' == 'HolaA')
+print('Hola' != 'HolaA')
+
+
+### Operadores Comparativos ###
+
+print(3 > 4 and 'Hola' > 'HolaA')
+print(3 > 4 or 'Hola' > 'HolaA')
+print(3 < 4 and 'Hola' < 'HolaA')
+print(3 < 4 or 'Hola' < 'HolaA')
+print(not(3 > 4))
+
+
