57a617846dc9779906e9a163b5d64cfad9ae019c
Akame <suefuxbbdudjdurbfud@gmail.com>
2023-11-13 00:25:54 -0500

    12/11/23

---
+++ 00_Hello.py
@@ -0,0 +1,29 @@
+# Esto es un comentario:
+# Nuestro hola mundo en Python
+
+"""
+Este es un
+comentario en
+varias lineas
+"""
+
+'''
+Este tambien es un
+comentario en
+varias lineas
+'''
+
+
+#Consultar el tipo de dato
+
+print(type('Hola World')) # Tipo 'str'
+
+print(type(5)) # Tipo 'int'
+
+print(type(1.3)) # Tipo 'float'
+
+print(type(True)) # Tipo 'bool'
+
+print(type(1 + 2j)) # Tipo 'complex'
+
+print(type(print('Cadena texto'))) # Tipo 'nonetype'
---
+++ 01_Variables.py
@@ -0,0 +1,18 @@
+# Variables
+
+# Buenas practicas de variables : first_name
+
+my_string = 'My String variable'
+print(my_string)
+
+my_int = 5
+print(my_int)
+
+my_float = 1.43
+print(my_float)
+
+my_bool = True
+print(my_bool)
+
+print(my_int, my_string)
+
